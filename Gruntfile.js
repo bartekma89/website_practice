3b5ad31e2ca5507a64108c75b70cd07344a44e1c
Bartek <bartekma89@gmail.com>
2017-10-14 12:54:05 +0200

    grunt example

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		//sass
+		sass: {
+			options: {
+				sourceMap: true,
+				style: 'expanded'
+			},
+			dist: {
+				files: {
+					'css/style.css': 'scss/style.scss'
+				}
+			}
+		},
+		//imagemin
+		imagemin: {
+			dynamic: {
+				options: {
+					optimalizationLevel: 7
+				},
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png, jpgm gif}', '!build/**/*.{png, jpgm gif}'],
+					dest: 'images/build'
+				}]
+			}
+		},
+		//watch
+		watch: {
+			scripts: {
+				files: ['scss/*.scss'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['sass', 'watch']);
+	grunt.registerTask('image', ['imagemin']);
+};
